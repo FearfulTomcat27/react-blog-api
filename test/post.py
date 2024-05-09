28d72e909bead514ec3deb19663fe2f202f173e0
FearfulTomcat <1471335448@qq.com>
2024-05-09 22:48:28 +0800

    删除test和http文件的跟踪

--- test/post.py
+++
@@ -1,7 +0,0 @@
-from services import post as crud
-from utils.database import  SessionLocal
-
-if __name__ == '__main__':
-    db = SessionLocal()
-    post = crud.get_posts(db)
-    print(post[0].date)
